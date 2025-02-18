37aa04f3b0c8ef7416452243a2a30ba6e8b6f86d
DaruriDheeraj <34548151+DaruriDheeraj@users.noreply.github.com>
2025-02-18 18:56:00 +0530

    Update cred2.py

--- cred2.py
+++ cred2.py
@@ -2,6 +2,6 @@ def login():
     """
     Function to prompt user for username and password, then checks if credentials are valid.
     """
-    password="abcde"
+    password="abcie"
 # Example usage
 login()
