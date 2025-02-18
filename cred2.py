686525c4556091e3d2cf62f36c59148c7e513500
DaruriDheeraj <34548151+DaruriDheeraj@users.noreply.github.com>
2025-02-18 14:57:59 +0530

    Update cred2.py

--- cred2.py
+++ cred2.py
@@ -2,14 +2,6 @@ def login():
     """
     Function to prompt user for username and password, then checks if credentials are valid.
     """
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-
-    # Replace with your actual validation logic based on a database or stored credentials
-    if username == "admin" and password == "password":
-        print("Login successful!")
-    else:
-        print("Invalid username or password.") 
-
+    password="abcde"
 # Example usage
 login()
